6eb162dfe1c89e9e21a7bc996ea4d734cc183526
Durval <th3scid@gmail.com>
2022-03-14 13:53:33 -0400

    first commit

---
+++ Djangochat/Chat/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+
+class Room(models.Model):
+    objects = None
+    name = models.CharField(max_length=1000)
+
+
+class Message(models.Model):
+    objects = None
+    value = models.CharField(max_length=10000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=10000000)
+    room = models.CharField(max_length=10000000)
+
